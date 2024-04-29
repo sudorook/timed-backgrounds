2ec29b802007e2164efe38be67fb6679977f2d35
sudorook <daemon@nullcodon.com>
2024-04-29 17:45:17 -0500

    add spdx data to all files (gpl 3.0 or later)

--- src/solar.h
+++ src/solar.h
@@ -1,21 +1,20 @@
-/* solar.h -- Solar position header
-   This file is part of Redshift.
-
-   Redshift is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   Redshift is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with Redshift.  If not, see <http://www.gnu.org/licenses/>.
-
-   Copyright (c) 2010  Jon Lund Steffensen <jonlst@gmail.com>
-*/
+// SPDX-FileCopyrightText: 2010 Jon Lund Steffensen <jonlst@gmail.com>
+// SPDX-FileCopyrightText: 2019 sudorook <daemon@nullcodon.com>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <https://www.gnu.org/licenses/>.
 
 #ifndef REDSHIFT_SOLAR_H
 #define REDSHIFT_SOLAR_H
